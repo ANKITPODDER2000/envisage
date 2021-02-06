60e79f2db303688540040cb9c9ef6ba40bfba3d4
Ankit Podder <ankitpodder0211@gmail.com>
2021-02-06 17:05:41 +0530

    home layout done for initial level

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,24 @@
 const overlay = document.querySelector("body > nav > div.overlay");
 const handburger = document.querySelector("body > nav > div.handburger");
+const cursor = document.querySelector("body > nav > div.overlay > .cursor");
+
 const toggleActive = () => {
     handburger.classList.toggle('active');
     overlay.classList.toggle('active');
+}
+
+const movemouse = e => {
+    // console.log(e.clientX , e.pageX)
+    cursor.style.left = (e.clientX - (window.innerWidth - overlay.offsetWidth)) + "px";
+    cursor.style.top = e.clientY + "px";
+}
+
+
+const mousein = t => {
+    // t.style.mixBlendMode = 'difference';
+    cursor.classList.add('active');
+}
+const mouseout = t => {
+    // t.style.mixBlendMode = 'none';
+    cursor.classList.remove('active');
 }
